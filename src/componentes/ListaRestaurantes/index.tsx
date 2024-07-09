cb9d3d79569236aad515d2da93919bc875db5668
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-09 10:57:44 +0100

    Lista de restaurantes

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,6 +1,8 @@
+import { useEffect } from 'react';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
+import axios from 'axios';
 
 const ListaRestaurantes = () => {
 
@@ -88,6 +90,17 @@ const ListaRestaurantes = () => {
       ]
     }
   ]
+
+useEffect(()=>{
+  //obter restaurantes
+  axios.get('http://0.0.0.0:8000/api/v1/restaurantes/')
+  .then(resposta =>{
+    console.log(resposta)
+  })
+  .catch(erro =>{
+    console.log(erro)
+  })
+},[])
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
