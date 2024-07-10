368bd0f7dec59cb9dbf39574d676b36f560b59b1
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-10 10:32:00 +0100

    ADM

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react"
 import IRestaurante from "../../../interfaces/IRestaurante"
 import {Button, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow, Table } from "@mui/material"
-import axios from "axios"
 import { Link } from "react-router-dom"
+import http from "../../../Http"
 const AdministracaoRestaurantes = () => {
 
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
     useEffect(() => {
-        axios.get<IRestaurante[]>('http://0.0.0.0:8000/api/v2/restaurantes/')
+        http.get<IRestaurante[]>('restaurantes/')
             .then(resposta => setRestaurantes(resposta.data))
     }, [])
 
     const excluir= (restauranteAhSerExcluido : IRestaurante) => {
-        axios.delete(`http://0.0.0.0:8000/api/v2/restaurantes/${restauranteAhSerExcluido.id}/`)
+        http.delete(`restaurantes/${restauranteAhSerExcluido.id}/`)
         .then(() => {
             const listaRestaurante = restaurantes.filter(restaurante => 
                 restaurante.id !== restauranteAhSerExcluido.id)
