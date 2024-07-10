e602657e3d743c94f3cbe6a7c26796062a60298f
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-10 10:24:23 +0100

    Lista de Restaurantes

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -2,18 +2,23 @@ import { useEffect, useState } from 'react';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { IPaginacao } from '../../interfaces/IPaginacao';
 
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
 
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
-  const [proxiomaPagina, setProximaPagina] = useState('')
+  const [proximaPagina, setProximaPagina] = useState('')
   const [paginaAnterior, setPaginaAnterior] = useState('')
+  const [busca, setBusca] = useState('')
 
-  const carregarDados = (url: string) => {
-    axios.get<IPaginacao<IRestaurante>>(url)
+  const carregarDados = (url: string,opcoes: AxiosRequestConfig = {}) => {
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
@@ -23,22 +28,27 @@ const ListaRestaurantes = () => {
         console.log(erro)
       })
   }
+  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    const opcoes = {
+      params: {
+
+      } as IParametrosBusca
+    }
+    if (busca) {
+      opcoes.params.search = busca
+    }
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }
 
   useEffect(() => {
-    //obter restaurantes
-    axios.get<IPaginacao<IRestaurante>>('http://0.0.0.0:8000/api/v1/restaurantes/')
-      .then(resposta => {
-        setRestaurantes(resposta.data.results)
-        setProximaPagina(resposta.data.next)
-        setPaginaAnterior(resposta.data.previous)
-      })
-      .catch(erro => {
-        console.log(erro)
-      })
+    // obter restaurantes
+    carregarDados('http://localhost:8000/api/v1/restaurantes/')
   }, [])
 
+
   const verMais = () => {
-    axios.get<IPaginacao<IRestaurante>>(proxiomaPagina)
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
       .then(resposta => {
         setRestaurantes([...restaurantes,...resposta.data.results])
         setProximaPagina(resposta.data.next)
@@ -49,12 +59,16 @@ const ListaRestaurantes = () => {
   }
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <form onSubmit={buscar}>
+      <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} />
+      <button type='submit'>buscar</button>
+    </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
       Página Anterior
     </button>}
-    {proxiomaPagina && <button onClick={verMais}>
-      ver mais
+    {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}>
+      Próxima página
     </button>}
   </section>)
 }
